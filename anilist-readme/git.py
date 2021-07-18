7bb3985ce5ee610b4b537e248f8f1b88a58e54f3
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-18 19:20:32 +0200

    bruh :sobbing:

--- anilist-readme/git.py
+++ anilist-readme/git.py
@@ -5,7 +5,7 @@ from shlex import quote
 def git_add_commit_push(readme_path: str, message: str, gh_token: str, email: str, username: str) -> None:
     # commit the change and push to your repo
     os.system(f'git config --global user.email {quote(email)}')
-    os.system(f'git config --global user.username {quote(username)}')
+    os.system(f'git config --global user.name {quote(username)}')
     os.system(
         f"git remote set-url origin https://{gh_token}@github.com/{os.environ['GITHUB_REPOSITORY']}.git")
     os.system(f"git add {readme_path}")
