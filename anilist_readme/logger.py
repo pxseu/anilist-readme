034014bb9da09bf37282eb6bbb4d2aba94e621ad
Kuba Ellwart <57842793+pxseu@users.noreply.github.com>
2021-10-31 03:13:15 +0100

    fix: use .get on environ

--- anilist_readme/logger.py
+++ anilist_readme/logger.py
@@ -2,7 +2,7 @@ import logging
 from os import environ
 
 logger = logging.getLogger("AniList")
-logger.setLevel(logging.DEBUG if environ["DEV"] == "true" else logging.INFO)
+logger.setLevel(logging.DEBUG if environ.get("DEV") == "true" else logging.INFO)
 
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter(fmt="%(asctime)s [%(levelname)s] - %(name)s: %(message)s", datefmt="%x %X"))
